2a355f35972109d5c61b432f9e9b1c454589a193
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-11-26 10:00:12 +0100

    Risk class added

---
+++ src/Risk.java
@@ -0,0 +1,14 @@
+public class Risk {
+    private String description = "";
+    private double probability = 0.0;
+    private double consequence = 0.0;
+    private double exposure = 0.0;
+
+    public void specifyRisk(String description, double probability, double consequence) {
+
+    }
+
+    private void calculateExposure() {
+
+    }
+}
