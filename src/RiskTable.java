5afd832e9953f4f5db727d227dd39bb6b648553e
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-11-26 10:21:10 +0100

    RiskTable added

---
+++ src/RiskTable.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class RiskTable {
+    private ArrayList<Risk> risks = new ArrayList<>();
+
+    public Risk addRisk(){
+        return null;
+    }
+
+    public void specifyRisk(Risk risk, String description, double probability, double consequence){
+
+    }
+}
