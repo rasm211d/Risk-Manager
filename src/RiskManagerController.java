dfcda613617729c8136aea39fd83c7897a2d0c13
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-11-26 10:23:22 +0100

    RiskManagerController

---
+++ src/RiskManagerController.java
@@ -0,0 +1,12 @@
+public class RiskManagerController {
+
+    RiskTable rt = new RiskTable();
+
+    public Risk addRisk(){
+        return null;
+    }
+
+    public void specifyRisk(Risk risk, String description, double probability, double consequence){
+
+    }
+}
