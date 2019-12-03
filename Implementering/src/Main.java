57eef39897e4490152e828afc7a95d0455e0febe
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-12-03 11:07:53 +0100

    javafx updateret

--- Implementering/src/Main.java
+++ Implementering/src/Main.java
@@ -9,8 +9,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Risk Manager");
+        primaryStage.setScene(new Scene(root, 640, 400));
         primaryStage.show();
     }
 
