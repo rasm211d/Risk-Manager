14168a343efa29f8a6d4faea0a9277d73a88ffe7
Rasmus Jannerup <54896192+rasm211d@users.noreply.github.com>
2019-12-03 09:52:52 +0100

    Added StrategyTable Class

--- Implementering/src/StrategyTable.java
+++ Implementering/src/StrategyTable.java
@@ -3,11 +3,17 @@ import java.util.ArrayList;
 public class StrategyTable {
     private ArrayList<Strategy> strategies = new ArrayList<>();
 
+    public Strategy addStrategy() {return null;}
+
     public Strategy showStrategy() {return null;}
 
     public Strategy deleteStrategy() {return null;}
 
-    public void sortStrategy (Strategy strategy, )
+    public void sortByCost (double cost){}
+
+    public void sortByResponsibility (String responsibility){}
+
+    public void sortByDescription (String description){}
 
     public void specifyStrategy (Strategy strategy, String description, double cost, String responsibility) {
         if (strategy == null) {
@@ -16,6 +22,6 @@ public class StrategyTable {
         strategy.specifyStrategy(description, cost, responsibility);
     }
 
-    public Strategy addStrategy() {return null;}
+
 
 }
