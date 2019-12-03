7d8b079be503993bbe3a17b9f4467ac93d91e24c
Kelvin-EASJ <kelvin.aggerholm@gmail.com>
2019-12-03 09:53:26 +0100

    Tilføjet strategies field til Risk klassen

--- Implementering/src/Risk.java
+++ Implementering/src/Risk.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+
 public class Risk {
     private String description = "";
     private double probability = 0.0;
     private double consequence = 0.0;
     private double exposure = 0.0;
+    private ArrayList<Strategy> strategies = new ArrayList<>();
 
     public void specifyRisk(String description, double probability, double consequence) {
         if (probability < 0.0 || probability > 1.0)
