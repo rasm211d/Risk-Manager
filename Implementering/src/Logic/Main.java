41c377348515eeae837ad63d205cae6d159562e2
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-12-04 14:34:36 +0100

    Sceneskifte fra startskærm til strategytable tilføjet

--- Implementering/src/Logic/Main.java
+++ Implementering/src/Logic/Main.java
@@ -2,21 +2,45 @@ package Logic;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GUI/start.fxml"));
-        primaryStage.setTitle("Logic.Risk Manager");
+        primaryStage.setTitle("Risk Manager");
         primaryStage.setScene(new Scene(root, 640, 400));
         primaryStage.show();
+
+//        Button strategybutton = new Button();
+//        strategybutton.setOnAction(value ->{});
     }
+
+
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    /** Method for changing scenes*/
+    public void changeScreenButtonPushed(javafx.event.ActionEvent actionEvent) throws Exception {
+
+        Parent strategytable = FXMLLoader.load(getClass().getClassLoader().getResource("GUI/strategytable.fxml"));
+        Scene strategytableScene = new Scene(strategytable);
+
+        // This is the line that gets the stage information
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(strategytableScene);
+        window.show();
     }
 }
