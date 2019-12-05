23fa5f706ffb6d74248fd68517574d5e94a03d61
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-12-05 09:29:11 +0100

    Main flyttet til GUI package

---
+++ src/Risk.java
@@ -0,0 +1,25 @@
+public class Risk {
+    private String description = "";
+    private double probability = 0.0;
+    private double consequence = 0.0;
+    private double exposure = 0.0;
+
+    public void specifyRisk(String description, double probability, double consequence) {
+        if (probability < 0.0 || probability > 1.0)
+            // throw exception
+            throw new InvalidProbabilityValueException();
+        if (consequence <= 0.0 || consequence > 20.0)
+            // throw exception
+            throw new InvalidConsequenceValueException();
+
+        this.description = description;
+        this.probability = probability;
+        this.consequence = consequence;
+
+        calculateExposure();
+    }
+
+    private void calculateExposure() {
+        exposure = probability * consequence;
+    }
+}
